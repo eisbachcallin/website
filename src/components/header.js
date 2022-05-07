da0a9929c0a0e35d0736db4d1190db6f2c791af0
vinckr <vincent@ory.sh>
2022-05-07 18:18:27 +0200

    u

--- src/components/header.js
+++ src/components/header.js
@@ -1,67 +1,33 @@
 import * as React from "react"
 
-import * as styles from './header.module.css'
+import * as styles from "./header.module.css"
 
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import Logo from "../images/logo.svg"
 
 const Header = ({ siteTitle, menuLinks }) => (
-  <header
-    style={{
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        id: "header",
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-        top: 0,
-        left: 0,
-        width: `250px`,
-      }}
-    >      
-    <StaticImage
-    className="logo"
-    layout="fixed"
-    formats={["auto", "webp", "avif"]}
-    src="../images/logo.png"
-    width={50}
-    height={50}
-    quality={95}
-    alt="Eisbach Callin Logo"
-  />
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+  <header>
+    <div>
+      {" "}
+      <Link to="/">
+        <img src={Logo} />
+      </Link>
       <div>
-          <nav>
-            <ul style={{ display: "flex", flex: 1, justifyContent: "flex-end" }}>
-              {menuLinks.map(link => (
-                <li
-                  key={link.name}
-                  style={{
-                    listStyleType: `none`,
-                    padding: `1rem`,
-                  }}
-                >
-                  <Link to={link.link}>
-                    {link.name}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </nav>
-        </div>
+        <ul
+          className={styles.nav}
+        >
+          {menuLinks.map(link => (
+            <li
+              key={link.name}
+            >
+              <Link to={link.link}>{link.name}</Link>
+            </li>
+          ))}
+        </ul>
+        <p>Munich's finest underground dance party since 2010</p>
+
+      </div>
     </div>
   </header>
 )
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,34 +1,40 @@
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import Header from "./header"
-import  Sidebar from "../components/sidebar"
-import * as styles from './layout.module.css'
-
+import Sidebar from "../components/sidebar"
+import * as styles from "./layout.module.css"
 
 const Layout = ({ location, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
-//  const classes = `global-wrapper grid`
+  //  const classes = `global-wrapper grid`
   const data = useStaticQuery(graphql`
-  query SiteTitleQuery {
-    site {
-      siteMetadata {
-        title 
-        menuLinks {
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+          menuLinks {
             name
             link
+          }
         }
       }
     }
-  }
-`)
+  `)
 
   return (
     <div className={styles.grid} data-is-root-path={isRootPath}>
-  <div className={styles.item1}>     <Header className="global-header" menuLinks={data.site.siteMetadata.menuLinks} siteTitle={data.site.siteMetadata?.title || `Title`} /></div>
-  <div className={styles.item2}>
-  <main>{children}</main></div>
-
+      <div className={styles.item1}>
+        {" "}
+        <Header
+          className="global-header"
+          menuLinks={data.site.siteMetadata.menuLinks}
+          siteTitle={data.site.siteMetadata?.title || `Title`}
+        />
+      </div>
+      <div className={styles.item2}>
+        <main>{children}</main>
+      </div>
 
       <footer>
         © {new Date().getFullYear()}, Built with
