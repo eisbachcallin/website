a928ed5fee666eb8cc1f45f2694ab323e3b6ee52
jfcurran <john@ory.sh>
2023-02-25 20:50:36 +0100

    updated layout grid for next on index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,43 @@
 import * as React from "react"
 import { Link, graphql } from "gatsby"
-import * as styles from "./index.module.css"
-
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import { GatsbyImage } from "gatsby-plugin-image"
+
+const seo = "Eisbach Callin | Underground Rave since 2010"
 
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
   const posts = data.allMarkdownRemark.nodes
   return (
     <Layout location={location} title={siteTitle}>
-      <Seo title="Eisbach Callin | Underground Rave since 2010" />
-      <div className={styles.postList}>
+      <Seo title={seo} />
+      <div className="grid grid-cols-2 gap-4 md:grid-cols-2 md:gap-4 lg:grid-cols-4 lg:gap-8">
         {posts.map(post => {
           const title = post.frontmatter.title || post.fields.slug
           return (
-            <Link to={post.fields.slug} key={post.fields.slug} itemProp="url">
+            <Link to={post.fields.slug} key={post.fields.slug} itemProp="url"
+                  className="text-white">
               <article
-                className={styles.postListItem}
+                className="relative"
                 itemScope
                 itemType="http://schema.org/Article"
               >
-                <h2>
-                  <span itemProp="headline">{title}</span>
-                </h2>
-                <p
-                  dangerouslySetInnerHTML={{
-                    __html: post.frontmatter.description || post.excerpt,
-                  }}
-                  itemProp="description"
-                />
-                <h3>{post.frontmatter.date}</h3>
                 <GatsbyImage
+                  className="aspect-din w-full object-fill transition ease-in duration-150"
                   image={post.frontmatter.cover.childImageSharp.gatsbyImageData}
+                  alt={post.frontmatter.description}
                 />
+                <div
+                  className="aspect-din w-full opacity-0 hover:opacity-100 absolute top-0 left-0 bg-white/0 p-4 backdrop-blur-lg">
+                  <h2 className="text-pink-500 drop-shadow">
+                    {title}
+                  </h2>
+                  <p itemProp="description">
+                    {post.frontmatter.description || post.excerpt}
+                  </p>
+                  <h3>{post.frontmatter.date}</h3>
+                </div>
               </article>
             </Link>
           )
