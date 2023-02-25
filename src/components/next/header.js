84c09cdfec9cfe02c64e62b6f5e981bb26bcc764
jfcurran <john@ory.sh>
2023-02-25 19:08:12 +0100

    update header, add marquee lmao

--- src/components/next/header.js
+++ src/components/next/header.js
@@ -4,9 +4,11 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import Header from "../header"
 
+const marqueeMessage = "Don't act like a clown, dance like one 🤡🥳"
+
 const HeaderNext = ({ siteTitle, menuLinks }) => (
-    <header className="bg-gray-900">
-      <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+    <header>
+      <nav className="flex items-center justify-between p-4 lg:px-8 lg:py-4" aria-label="Global">
         <div className="flex lg:flex-1">
           <Link to="/" className="-m-1.5 p-1.5">
             <span className="sr-only">{siteTitle}</span>
@@ -15,12 +17,27 @@ const HeaderNext = ({ siteTitle, menuLinks }) => (
         </div>
         <div className="flex gap-x-8 md:gap-x-12">
           {menuLinks.map(link => (
-            <div className="text-sm md:text-base font-semibold leading-6 text-white" key={link.name}>
+            <div className="text-sm font-regular leading-6 text-white hover:text-pink-500 uppercase " key={link.name}>
               <Link to={link.link}>{link.name}</Link>
             </div>
           ))}
         </div>
       </nav>
+      <div className="flex">
+        <marquee className="text-sm text-white py-2 border-y border-white" behaviour="scroll">
+          <span className="sr-only">{marqueeMessage}</span>
+          {marqueeMessage}
+        </marquee>
+        <marquee className="hidden md:block text-sm text-white py-2 border-y border-white" behaviour="scroll">
+          {marqueeMessage}
+        </marquee>
+        <marquee className="hidden lg:block text-sm text-white py-2 border-y border-white" behaviour="scroll">
+          {marqueeMessage}
+        </marquee>
+        <marquee className="hidden lg:block text-sm text-white py-2 border-y border-white" behaviour="scroll">
+          {marqueeMessage}
+        </marquee>
+      </div>
     </header>
 )
 
