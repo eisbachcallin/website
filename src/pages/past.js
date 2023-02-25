16aafdbacf446d5e16f3f6547b88541d14154d77
jfcurran <john@ory.sh>
2023-02-25 21:06:25 +0100

    update /past to render images in grid

--- src/pages/past.js
+++ src/pages/past.js
@@ -5,45 +5,90 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 import { GatsbyImage } from "gatsby-plugin-image"
 
-const Past = ({ data, location }) => {
+const seo = "Past Events | Eisbach Callin"
+
+// const Past = ({ data, location }) => {
+//   const siteTitle = data.site.siteMetadata?.title || `Title`
+//   const posts = data.allMarkdownRemark.nodes
+//
+//   return (
+//     <Layout location={location} title={siteTitle}>
+//       <Seo title="Past Events | Eisbach Callin" />
+//       <h2>Past Events</h2>
+//       <ol className="post-list">
+//         {posts.map(post => {
+//           const title = post.frontmatter.title || post.fields.slug
+//           return (
+//             <li key={post.fields.slug}>
+//               <article
+//                 className="post-list-item"
+//                 itemScope
+//                 itemType="http://schema.org/Article"
+//               >
+//                 <header>
+//                   <h3>
+//                     <Link to={post.fields.slug} itemProp="url">
+//                       <span itemProp="headline">{title}</span>
+//                     </Link>
+//                   </h3>
+//                   <small>{post.frontmatter.date}</small>
+//                 </header>
+//                 <GatsbyImage
+//                   image={post.frontmatter.cover.childImageSharp.gatsbyImageData}
+//                 />
+//               </article>
+//             </li>
+//           )
+//         })}
+//       </ol>
+//     </Layout>
+//   )
+// }
+//
+// export default Past
+
+const ebcPast = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
   const posts = data.allMarkdownRemark.nodes
-
   return (
     <Layout location={location} title={siteTitle}>
-      <Seo title="Past Events | Eisbach Callin" />
-      <h2>Past Events</h2>
-      <ol className="post-list">
+      <Seo title={seo} />
+      <div className="grid grid-cols-2 gap-4 md:grid-cols-2 md:gap-4 lg:grid-cols-4 lg:gap-8">
         {posts.map(post => {
           const title = post.frontmatter.title || post.fields.slug
           return (
-            <li key={post.fields.slug}>
+            <Link to={post.fields.slug} key={post.fields.slug} itemProp="url"
+                  className="text-white">
               <article
-                className="post-list-item"
+                className="relative"
                 itemScope
                 itemType="http://schema.org/Article"
               >
-                <header>
-                  <h3>
-                    <Link to={post.fields.slug} itemProp="url">
-                      <span itemProp="headline">{title}</span>
-                    </Link>
-                  </h3>
-                  <small>{post.frontmatter.date}</small>
-                </header>
                 <GatsbyImage
+                  className="aspect-din w-full object-fill transition ease-in duration-150"
                   image={post.frontmatter.cover.childImageSharp.gatsbyImageData}
+                  alt={post.frontmatter.description}
                 />
+                <div
+                  className="aspect-din w-full opacity-0 hover:opacity-100 absolute top-0 left-0 bg-white/0 p-4 backdrop-blur-lg">
+                  <h2 className="text-pink-500 drop-shadow">
+                    {title}
+                  </h2>
+                  <p itemProp="description">
+                    {post.frontmatter.description || post.excerpt}
+                  </p>
+                  <h3>{post.frontmatter.date}</h3>
+                </div>
               </article>
-            </li>
+            </Link>
           )
         })}
-      </ol>
+      </div>
     </Layout>
   )
 }
 
-export default Past
+export default ebcPast
 
 export const pageQuery = graphql`
   query {
