65a3fb2b8deb73cf27eeae80062513dce9e0c2e0
jfcurran <john@ory.sh>
2023-02-25 20:49:05 +0100

    header updated

--- src/components/next/header.js
+++ src/components/next/header.js
@@ -7,8 +7,8 @@ import Header from "../header"
 const marqueeMessage = "Don't act like a clown, dance like one 🤡🥳"
 
 const HeaderNext = ({ siteTitle, menuLinks }) => (
-    <header>
-      <nav className="flex items-center justify-between p-4 lg:px-8 lg:py-4" aria-label="Global">
+    <header id={"header"}>
+      <nav id={"header-nav"} className="fixed z-10 flex w-screen items-center justify-between p-4 lg:px-8 lg:py-4 backdrop-blur-lg bg-black/20 border-b border-black" aria-label="Global">
         <div className="flex lg:flex-1">
           <Link to="/" className="-m-1.5 p-1.5">
             <span className="sr-only">{siteTitle}</span>
@@ -17,24 +17,24 @@ const HeaderNext = ({ siteTitle, menuLinks }) => (
         </div>
         <div className="flex gap-x-8 md:gap-x-12">
           {menuLinks.map(link => (
-            <div className="text-sm font-regular leading-6 text-white hover:text-pink-500 uppercase " key={link.name}>
+            <div className="text-sm font-regular leading-6 text-white hover:text-pink-500 uppercase" key={link.name}>
               <Link to={link.link}>{link.name}</Link>
             </div>
           ))}
         </div>
       </nav>
-      <div className="flex">
-        <marquee className="text-sm text-white py-2 border-y border-white" behaviour="scroll">
+      <div id={"header-marquee"} className="flex pt-20">
+        <marquee className="text-sm text-white py-2 border-y border-gray-900" behaviour="scroll">
           <span className="sr-only">{marqueeMessage}</span>
           {marqueeMessage}
         </marquee>
-        <marquee className="hidden md:block text-sm text-white py-2 border-y border-white" behaviour="scroll">
+        <marquee className="hidden md:block text-sm text-white py-2 border-y border-gray-900" behaviour="scroll">
           {marqueeMessage}
         </marquee>
-        <marquee className="hidden lg:block text-sm text-white py-2 border-y border-white" behaviour="scroll">
+        <marquee className="hidden lg:block text-sm text-white py-2 border-y border-gray-900" behaviour="scroll">
           {marqueeMessage}
         </marquee>
-        <marquee className="hidden lg:block text-sm text-white py-2 border-y border-white" behaviour="scroll">
+        <marquee className="hidden lg:block text-sm text-white py-2 border-y border-gray-900" behaviour="scroll">
           {marqueeMessage}
         </marquee>
       </div>
