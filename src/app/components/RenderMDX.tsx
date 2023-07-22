9d888ddd342318ea6857086aa75b0f0cec8a24fe
jfcurran <john@ory.sh>
2023-07-22 16:10:11 +0200

    add component that renders mdx content RenderMDX.tsx

---
+++ src/app/components/RenderMDX.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react'
+import { useMDXComponent } from 'next-contentlayer/hooks'
+import Image from 'next/image'
+
+const components = {
+  Image,
+}
+
+interface MdxProps {
+  code: string
+  className?: string
+}
+
+export function RenderMdx({ code, className }: MdxProps) {
+  const Component = useMDXComponent(code)
+
+  return (
+    <div className='mdx prose prose-sm dark:prose-invert md:prose-base'>
+      <Component className={className} components={components} />
+    </div>
+  )
+}
