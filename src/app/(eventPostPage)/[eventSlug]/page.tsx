c50218f400482034d07910655cda818199924dfd
jfcurran <john@ory.sh>
2023-07-22 16:11:14 +0200

    render static page and slug for each post, use mdxMetadata in head

---
+++ src/app/(eventPostPage)/[eventSlug]/page.tsx
@@ -0,0 +1,42 @@
+import { allEvents } from 'contentlayer/generated'
+import React from 'react'
+import { notFound } from 'next/navigation'
+import { RenderMdx } from '@/app/components/RenderMDX'
+import { Metadata } from 'next'
+
+interface PageProps {
+  params: {
+    eventSlug: string
+  }
+}
+
+async function getPostFromParams(slug: string) {
+  const examplePost = allEvents.find((post) => post.slugAsParams === slug)
+  if (!examplePost) notFound()
+  return examplePost
+}
+
+export const generateStaticParams = async () =>
+  allEvents.map((post) => ({ slug: post.slugAsParams }))
+
+export async function generateMetadata({
+  params,
+}: PageProps): Promise<Metadata> {
+  const post = await getPostFromParams(params.eventSlug)
+  return {
+    title: post.mdxMeta.title,
+    description: post.mdxMeta.description,
+  }
+}
+
+const ExamplePostPage = async ({ params }: PageProps) => {
+  const post = await getPostFromParams(params.eventSlug)
+
+  return (
+    <article className='mx-auto max-w-5xl p-5'>
+      <RenderMdx code={post.body.code} />
+    </article>
+  )
+}
+
+export default ExamplePostPage
