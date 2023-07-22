0e2f68e7eca05deb0ee0d9c8cdbbdd9bc0fff634
jfcurran <john@ory.sh>
2023-07-22 15:27:24 +0200

    add map of all events to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
 import Image from 'next/image'
 
+import { allEvents } from 'contentlayer/generated'
+import { compareDesc } from 'date-fns'
+import EventCard from './components/EventCard'
+
 export default function Home() {
+  const examplePosts = allEvents.sort((a, b) =>
+    compareDesc(new Date(a.date), new Date(b.date))
+  )
+
   return (
-    <main>
-      <h1>home</h1>
-    </main>
+    <div className='mx-auto my-4 max-w-5xl'>
+      {examplePosts.map((post, _id) => (
+        <EventCard key={_id} eventPost={post} />
+      ))}
+    </div>
   )
 }
