75ceb76a96afe6a3f1808671cbb04fa229f8c4b9
jfcurran <john@ory.sh>
2023-07-27 19:26:19 +0200

    add layout to next events page, add fallback in case there are no events

--- src/app/next/page.tsx
+++ src/app/next/page.tsx
@@ -1,5 +1,5 @@
 import { allEvents, Event } from 'contentlayer/generated'
-import { compareDesc, isAfter, isBefore, addDays } from 'date-fns'
+import { compareDesc, isAfter, addDays } from 'date-fns'
 import EventCard from '../components/EventCard'
 import Link from 'next/link'
 
@@ -19,27 +19,46 @@ export default function NextEventsPage() {
       }
     })
 
-  if (futureEvents.length === 0) {
-    return (
-      <div className='mx-auto my-4 max-w-5xl'>
-        <h1>No Future Events</h1>
-        <p>There are currently no upcoming events. Check our past events:</p>
-
-        {pastEvents.slice(0, 3).map((event) => (
-          <EventCard key={event._id} eventPost={event} />
-        ))}
-
-        <a href='/past'>See all previous events</a>
-      </div>
-    )
-  }
-
   return (
-    <div className='mx-auto my-4 max-w-5xl'>
-      <h1>Future Events</h1>
-      {futureEvents.map((event) => (
-        <EventCard key={event._id} eventPost={event} />
-      ))}
+    <div className='mx-auto w-full px-2 py-12'>
+      <div className='flex flex-col items-start justify-center gap-6 xl:mx-auto xl:grid xl:max-w-max xl:grid-cols-2'>
+        <h1 className='xl:grid-col-1 mx-auto w-full max-w-max font-sans text-6xl font-bold uppercase leading-none tracking-tighter text-black sm:py-16 sm:text-8xl xl:sticky xl:top-0 xl:max-w-none'>
+          don’t act like a clown, dance like one
+        </h1>
+        <div className='xl:max-w-1/2 xl:grid-col-1 mx-auto max-w-max space-y-8 xl:max-w-none xl:py-16'>
+          {futureEvents.length > 0 ? (
+            <div className='space-y-2'>
+              <h2 className='text-sm font-bold uppercase leading-none'>
+                next ↓ ↓ ↓
+              </h2>
+              <div className='grid w-full grid-cols-1 gap-4 sm:grid-cols-2'>
+                {futureEvents.map((event) => (
+                  <EventCard key={event._id} eventPost={event} />
+                ))}
+              </div>
+            </div>
+          ) : (
+            <div className='space-y-2'>
+              <h2 className='text-sm font-bold uppercase leading-none'>
+                No Future Events
+              </h2>
+              <p>There are currently no upcoming events. Check back later.</p>
+            </div>
+          )}
+          {pastEvents.length < 0 ? (
+            <div className='space-y-2'>
+              <h2 className='text-sm font-bold uppercase leading-none'>
+                past ↓ ↓ ↓
+              </h2>
+              <div className='grid w-full grid-cols-1 gap-4 sm:grid-cols-2 xl:grid-cols-3 '>
+                {pastEvents.slice(0, 3).map((event) => (
+                  <EventCard key={event._id} eventPost={event} />
+                ))}
+              </div>
+            </div>
+          ) : null}
+        </div>
+      </div>
     </div>
   )
 }
