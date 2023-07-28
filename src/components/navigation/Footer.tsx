3858aede87f6e5f099aac32f74ecdb459b286e93
jfcurran <john@ory.sh>
2023-07-28 16:25:46 +0200

    adjust styling on Footer.tsx

--- src/components/navigation/Footer.tsx
+++ src/components/navigation/Footer.tsx
@@ -10,11 +10,11 @@ type FooterProps = {
 const Footer: React.FC<FooterProps> = ({ className }) => {
   return (
     <footer
-      className={clsx('border-t border-pink-500 bg-black ', className)}
+      className={clsx('border-t border-black bg-white', className)}
       aria-label='Global'
     >
       <div className='mx-auto flex h-12 max-w-max items-center justify-end p-2'>
-        <p className='text-sm font-light uppercase leading-none text-white'>
+        <p className='text-sm font-light uppercase leading-none text-black'>
           eisbach callin ©{getYear(new Date())}
         </p>
       </div>
