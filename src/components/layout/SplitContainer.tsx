4c5972cf4284c537900af3ad2422c1c3d8a26b5a
jfcurran <john@ory.sh>
2023-08-03 22:25:55 +0200

    improve layout of SplitContainer

--- src/components/layout/SplitContainer.tsx
+++ src/components/layout/SplitContainer.tsx
@@ -17,16 +17,24 @@ const SplitContainer: React.FC<SplitContainerProps> = ({
   return (
     <div
       className={clsx(
-        'mx-auto grid max-w-max gap-4 p-2 xl:grid-cols-2',
+        'mx-auto grid max-w-max flex-grow xl:grid-cols-3 xl:border-x xl:border-black',
         className
       )}
     >
-      <div className='xl:grid-col-1 mx-auto w-full'>
-        <div className={clsx('sm:py-16', { 'xl:sticky xl:top-0': stickyLeft })}>
+      <div
+        className={clsx(
+          'col-span-full mx-auto w-full border-b border-black py-8 text-black xl:col-span-1 xl:border-b-0 xl:border-r xl:border-black xl:py-0'
+        )}
+      >
+        <div
+          className={clsx('p-2', {
+            'xl:sticky xl:top-10': stickyLeft,
+          })}
+        >
           {leftSide}
         </div>
       </div>
-      <div>{rightSide}</div>
+      <div className='col-span-full xl:col-span-2'>{rightSide}</div>
     </div>
   )
 }
