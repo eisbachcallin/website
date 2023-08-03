ac99bb3f6eb96ef548b53778fb24a294806723eb
jfcurran <john@ory.sh>
2023-08-03 22:24:33 +0200

    improve grid layout on EventSection

--- src/components/EventSection.tsx
+++ src/components/EventSection.tsx
@@ -1,3 +1,4 @@
+// EventSection
 import clsx from 'clsx'
 import EventCard from '@/components/EventCard'
 import { Event } from 'contentlayer/generated'
@@ -8,16 +9,17 @@ interface EventSectionProps {
 }
 
 const EventSection = ({ events, label }: EventSectionProps) => {
-  const gridClasses = clsx('grid w-full gap-4', {
-    'xl:grid-cols-2': events.length < 2,
-    'sm:grid-cols-2 xl:grid-cols-3': events.length > 2,
-  })
-
   return (
-    <section className='space-y-2'>
-      <h2 className='text-sm font-light uppercase leading-none'>{label}</h2>
+    <section className='flex flex-col gap-y-2 p-2 xl:grid xl:grid-cols-6'>
+      <h2 className='text-sm font-light uppercase leading-none xl:col-span-1'>
+        {label}
+      </h2>
       {events.length > 0 ? (
-        <div className={clsx(gridClasses, 'sm:grid-cols-2')}>
+        <div
+          className={
+            'grid w-full gap-2 sm:grid-cols-2 xl:col-span-5 xl:grid-cols-4'
+          }
+        >
           {events.map((event) => (
             <EventCard key={event._id} eventPost={event} />
           ))}
