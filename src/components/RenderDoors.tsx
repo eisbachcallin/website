75d2dc8384b566590e0d2ffb32fc68f508a201cf
jfcurran <john@ory.sh>
2023-08-04 23:43:52 +0200

    add mdx component to render time

---
+++ src/components/RenderDoors.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+interface DoorsProps {
+  timeDoors: string
+  price: string
+  before?: {
+    timeBefore: string
+    priceBefore: string
+  }
+}
+
+const RenderDoors: React.FC<DoorsProps> = ({ timeDoors, price, before }) => {
+  return (
+    <div className='flex space-x-1 font-mono uppercase'>
+      <div>Doors {timeDoors}h</div>
+      <div className='text-pink-500'>/</div>
+      <div className='font-medium'>{price}€</div>
+      {before && (
+        <>
+          <div className='text-pink-500'>/</div>
+          <div>
+            {before.priceBefore}€ before {before.timeBefore}
+          </div>
+        </>
+      )}
+    </div>
+  )
+}
+
+export default RenderDoors
