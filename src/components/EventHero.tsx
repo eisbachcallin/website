82157129efe7fbf91d0baa0d6e1917fe51bcfd01
jfcurran <john@ory.sh>
2023-08-04 21:36:18 +0200

    update EventHero

--- src/components/EventHero.tsx
+++ src/components/EventHero.tsx
@@ -11,33 +11,23 @@ const EventHero: React.FC<PostHeroProps> = ({ post }) => {
   const { mdxMeta, cover, date } = post
 
   return (
-    <div className='mb-8 space-y-8'>
-      <div className='mx-auto grid grid-cols-12 gap-4'>
-        <div className='col-span-12 flex flex-wrap items-baseline gap-x-4'>
-          <time
-            dateTime={date}
-            className='shrink-0 text-xs leading-loose text-gray-500'
-          >
-            {format(parseISO(date), 'LLLL d, yyyy')}
+    <div className='grid gap-y-8'>
+      <div className='flex flex-col gap-y-2 pb-2 font-sans text-3xl font-semibold'>
+        <h1 className='text-gray-950 dark:text-white '>{mdxMeta.title}</h1>
+        <div className='flex flex-wrap items-baseline gap-x-4'>
+          <time dateTime={date} className='bg-pink-500 p-[0.05rem] text-white'>
+            {format(parseISO(date), 'dd-MM-yyyy')}
           </time>
-          <span className='text-xs text-pink-500'>/</span>
         </div>
-
-        <h1 className='col-span-12 text-3xl font-semibold text-gray-950 dark:text-white md:col-span-7 md:col-start-1'>
-          {mdxMeta.title}
-        </h1>
       </div>
       <Image
         priority
         width={860}
         height={860}
         src={cover}
-        alt={mdxMeta.description}
-        className='aspect-din w-full rounded object-cover'
+        alt={mdxMeta.title}
+        className='aspect-din w-full border border-black object-cover'
       />
-      <p className='text-xl text-gray-700 dark:text-gray-200'>
-        {mdxMeta.description}
-      </p>
     </div>
   )
 }
