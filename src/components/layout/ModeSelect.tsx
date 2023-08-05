e03607f8fb6d35d6452ed01de1561ee0140b8f34
jfcurran <john@ory.sh>
2023-08-05 16:07:15 +0200

    add ModeSelect

--- src/components/layout/ModeSelect.tsx
+++ src/components/layout/ModeSelect.tsx
@@ -1,59 +1,44 @@
 'use client'
 
+import { useEffect, useState } from 'react'
+
 const THEME_LIGHT = 'theme-light'
 const THEME_DARK = 'theme-dark'
 
-function SunIcon(props: any) {
-  return (
-    <svg viewBox='0 0 20 20' fill='none' aria-hidden='true' {...props}>
-      <path d='M12.5 10a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Z' />
-      <path
-        strokeLinecap='round'
-        d='M10 5.5v-1M13.182 6.818l.707-.707M14.5 10h1M13.182 13.182l.707.707M10 15.5v-1M6.11 13.889l.708-.707M4.5 10h1M6.11 6.111l.708.707'
-      />
-    </svg>
-  )
-}
+export function ModeSelect() {
+  const [isDarkMode, setIsDarkMode] = useState(false)
 
-function MoonIcon(props: any) {
-  return (
-    <svg viewBox='0 0 20 20' fill='none' aria-hidden='true' {...props}>
-      <path d='M15.224 11.724a5.5 5.5 0 0 1-6.949-6.949 5.5 5.5 0 1 0 6.949 6.949Z' />
-    </svg>
-  )
-}
-
-export function ModeToggle() {
-  function disableTransitionsTemporarily() {
-    document.documentElement.classList.add('[&_*]:!transition-none')
-    window.setTimeout(() => {
-      document.documentElement.classList.remove('[&_*]:!transition-none')
-    }, 0)
-  }
-
-  function toggleMode() {
-    disableTransitionsTemporarily()
-
-    // toggle theme on <html> element
-    const classes = document.documentElement.className.split(' ')
-    if (classes.includes(THEME_LIGHT)) {
-      document.documentElement.classList.remove(THEME_LIGHT)
+  useEffect(() => {
+    const theme = window.localStorage.getItem('theme')
+    const currentMode = theme
+      ? theme === THEME_DARK
+      : document.documentElement.classList.contains(THEME_DARK)
+    setIsDarkMode(currentMode)
+    if (currentMode) {
       document.documentElement.classList.add(THEME_DARK)
     } else {
-      document.documentElement.classList.remove(THEME_DARK)
       document.documentElement.classList.add(THEME_LIGHT)
     }
+  }, [])
+
+  function toggleMode() {
+    const nextTheme = isDarkMode ? THEME_LIGHT : THEME_DARK
+    setIsDarkMode(!isDarkMode)
+    document.documentElement.classList.remove(
+      isDarkMode ? THEME_DARK : THEME_LIGHT
+    )
+    document.documentElement.classList.add(nextTheme)
+    window.localStorage.setItem('theme', nextTheme)
   }
 
   return (
     <button
       type='button'
-      className='inline-flex items-center justify-center rounded-md transition hover:stroke-accent '
+      className='inline-flex items-center justify-center bg-accent uppercase text-onaccent'
       aria-label='Toggle color mode'
       onClick={toggleMode}
     >
-      <SunIcon className='h-6 w-6 stroke-default hover:stroke-accent dark:hidden' />
-      <MoonIcon className='hidden h-6 w-6 stroke-default hover:stroke-accent dark:block' />
+      {isDarkMode ? 'Light' : 'Dark'}
     </button>
   )
 }
