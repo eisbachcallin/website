79e5e40578ba3f9d18d88a39f0a3974b9b42a586
jfcurran <john@ory.sh>
2023-08-06 16:26:20 +0200

    add 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,43 +1,66 @@
-import React from 'react'
+'use client'
+
+import React, { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
 import Link from 'next/link'
 import SplitContainer from '@/components/layout/SplitContainer'
-import ResidentSection from '@/components/ResidentSection'
 import Image from 'next/image'
 import { Metadata } from 'next'
 
 export const metadata: Metadata = {
-  title: '404',
+  title: '404 - Event not found',
+  description: 'The requested event could not be found.',
 }
 
 export default function NotFound() {
+  const router = useRouter()
+
+  useEffect(() => {
+    router.push('/404')
+  }, [router])
+
   return (
-    <SplitContainer
-      stickyLeft
-      leftSide={
-        <div className='space-y-8 object-cover font-sans sm:text-8xl xl:space-y-16'>
-          <div className='text-2xl sm:text-3xl'>
-            <h1>
-              404 / This page doesn’t exist. Check{' '}
-              <Link className='text-accent hover:text-default' href='/'>
-                home
-              </Link>{' '}
-              to find all upcoming or past Eisbach Callin events.
-            </h1>
+    <>
+      <title>404 - Event not found | Eisbach Callin</title>
+      <meta name='description' content='Underground Rave since 2010'></meta>
+      <link
+        rel='icon'
+        href='/icon.svg'
+        type='image/svg+xml'
+        sizes='256x256'
+      ></link>
+      <meta
+        name='viewport'
+        content='width=device-width, initial-scale=1'
+      ></meta>
+      <SplitContainer
+        stickyLeft
+        leftSide={
+          <div className='space-y-8 object-cover font-sans sm:text-8xl xl:space-y-16'>
+            <div className='text-2xl sm:text-3xl'>
+              <h1>
+                404 / This page doesn’t exist. Check{' '}
+                <Link className='text-accent hover:text-default' href='/'>
+                  home
+                </Link>{' '}
+                to find all upcoming or past Eisbach Callin events.
+              </h1>
+            </div>
           </div>
-        </div>
-      }
-      rightSide={
-        <div className='p-2'>
-          <Image
-            priority
-            width={800}
-            height={500}
-            src='/about/group.jpg'
-            alt='foobar'
-            className='mb-2 w-full border border-default object-cover'
-          />
-        </div>
-      }
-    />
+        }
+        rightSide={
+          <div className='p-2'>
+            <Image
+              priority
+              width={800}
+              height={500}
+              src='/error/404.jpg'
+              alt='The requested event could not be found'
+              className='w-full border border-default object-cover'
+            />
+          </div>
+        }
+      />
+    </>
   )
 }
