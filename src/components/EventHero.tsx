003502ebb78b8ce12afadf8dd754fdd0fa479fe8
jfcurran <john@ory.sh>
2023-08-15 12:29:24 +0200

    change behaviour and styles of EventHero

--- src/components/EventHero.tsx
+++ src/components/EventHero.tsx
@@ -11,20 +11,7 @@ const EventHero: React.FC<PostHeroProps> = ({ post }) => {
   const { mdxMeta, cover, date } = post
 
   return (
-    <div className='grid gap-y-8'>
-      <div className='flex flex-col gap-y-2 pb-2 font-semibold'>
-        <h1 className='font-sans text-2xl text-default sm:text-3xl'>
-          {mdxMeta.title}
-        </h1>
-        <div className='flex flex-wrap items-baseline gap-x-4'>
-          <time
-            dateTime={date}
-            className='bg-accent p-[0.05rem] font-mono text-2xl text-onaccent sm:text-3xl'
-          >
-            {format(parseISO(date), 'dd-MM-yyyy')}
-          </time>
-        </div>
-      </div>
+    <div className='grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2 xl:grid-cols-1'>
       <Image
         priority
         width={860}
@@ -33,6 +20,14 @@ const EventHero: React.FC<PostHeroProps> = ({ post }) => {
         alt={mdxMeta.title}
         className='aspect-din w-full border border-default object-cover'
       />
+      <div className='flex flex-col justify-start gap-y-2 pb-2 font-sans'>
+        <h1 className='text-2xl text-default sm:text-3xl '>{mdxMeta.title}</h1>
+        <div className='flex flex-wrap items-baseline gap-x-4'>
+          <time dateTime={date} className='text-2xl text-accent sm:text-3xl'>
+            {format(parseISO(date), 'dd-MM-yyyy')}
+          </time>
+        </div>
+      </div>
     </div>
   )
 }
