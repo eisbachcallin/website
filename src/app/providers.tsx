10426ab24d8db39e99dd8fa4d05b6d443b298325
jfcurran <john@ory.sh>
2023-08-15 12:02:47 +0200

    add default system theme

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,5 +3,9 @@
 import { ThemeProvider } from 'next-themes'
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <ThemeProvider defaultTheme='light'>{children}</ThemeProvider>
+  return (
+    <ThemeProvider defaultTheme='system' enableSystem>
+      {children}
+    </ThemeProvider>
+  )
 }
