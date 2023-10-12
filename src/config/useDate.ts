aaa19958040582a2a797ac1a1f4bd15541001c9c
John Curran <johnfitzgeraldcurran@gmail.com>
2023-10-13 00:45:30 +0200

    fix: useDate now adds two days to upcoming event

--- src/config/useDate.ts
+++ src/config/useDate.ts
@@ -1,8 +1,8 @@
 import { allEvents, Event } from 'contentlayer/generated'
-import { compareDesc, addDays, isAfter } from 'date-fns'
+import { compareDesc, addDays, isBefore } from 'date-fns'
 
 export function useDate() {
-  const latentDay = addDays(new Date(), 2)
+  const cutOffDate = addDays(new Date(), -2)
 
   let futureEvents: Event[] = []
   let pastEvents: Event[] = []
@@ -10,10 +10,10 @@ export function useDate() {
   ;(allEvents as Event[])
     .sort((a, b) => compareDesc(new Date(a.date), new Date(b.date)))
     .forEach((event) => {
-      if (isAfter(new Date(event.date), latentDay)) {
-        futureEvents.push(event)
-      } else {
+      if (isBefore(new Date(event.date), cutOffDate)) {
         pastEvents.push(event)
+      } else {
+        futureEvents.push(event)
       }
     })
 
