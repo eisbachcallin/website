76a91f78c6d43edc08d9c89593c6fccd5e6139ca
John Curran <johnfitzgeraldcurran@gmail.com>
2025-02-01 18:21:27 -0300

    refactor: remove unused imports and font configurations in layout

--- src/app/camp/layout.tsx
+++ src/app/camp/layout.tsx
@@ -1,21 +1,5 @@
-import Footer from '@/components/navigation/Footer'
-import Header from '@/components/navigation/Header'
-import clsx from 'clsx'
-import { Share_Tech_Mono as Mono, Inter as Sans } from 'next/font/google'
 import '../globals.css'
-import { Providers } from '../../app/providers'
 import { Metadata } from 'next'
-
-const sans = Sans({
-  subsets: ['latin'],
-  variable: '--font-sans',
-})
-
-const mono = Mono({
-  subsets: ['latin'],
-  weight: ['400'],
-  variable: '--font-mono',
-})
 
 export const metadata: Metadata = {
   title: 'Eisbach Callin Camp',
